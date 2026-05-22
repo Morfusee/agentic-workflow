7e2d376ebe1e30af5d5605410b2ea11b48b16736
Morfusee <markvalenzuela.coding@gmail.com>
2026-05-22 19:34:08 +0800

    feat(nvim): add markdown support with preview and linting

--- configs/nvim/lua/config/autocmds.lua
+++ configs/nvim/lua/config/autocmds.lua
@@ -6,3 +6,26 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    vim.diagnostic.config({
+      virtual_text = {
+        severity = {
+          min = vim.diagnostic.severity.ERROR,
+        },
+      },
+      signs = {
+        severity = {
+          min = vim.diagnostic.severity.ERROR,
+        },
+      },
+      underline = {
+        severity = {
+          min = vim.diagnostic.severity.ERROR,
+        },
+      },
+    })
+  end,
+})
