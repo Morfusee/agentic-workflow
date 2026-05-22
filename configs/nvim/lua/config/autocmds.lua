011e323e109ad1fba8630fb4d018812946e95550
Morfusee <markvalenzuela.coding@gmail.com>
2026-05-22 19:44:25 +0800

    feat(nvim): disable spell checking globally and for specific filetypes

--- configs/nvim/lua/config/autocmds.lua
+++ configs/nvim/lua/config/autocmds.lua
@@ -7,6 +7,9 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Disable LazyVim's default wrap/spell behavior for markdown/text files
+pcall(vim.api.nvim_del_augroup_by_name, "lazyvim_wrap_spell")
+
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
@@ -27,5 +30,12 @@ vim.api.nvim_create_autocmd("User", {
         },
       },
     })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text", "gitcommit" },
+  callback = function()
+    vim.opt_local.spell = false
   end,
 })
