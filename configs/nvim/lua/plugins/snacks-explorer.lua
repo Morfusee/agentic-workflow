5ac99a3495da17242fc514feac1828efe3bbfc8c
Morfusee <markvalenzuela.coding@gmail.com>
2026-06-25 14:43:58 +0800

    feat(nvim): add snacks.nvim explorer sidebar and update plugin locks
    - add snacks-explorer plugin for file tree sidebar on the right
    
    - update lazy-lock.json with latest plugin versions

---
+++ configs/nvim/lua/plugins/snacks-explorer.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            layout = {
+              preset = "sidebar",
+              preview = false,
+              layout = {
+                position = "right",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
